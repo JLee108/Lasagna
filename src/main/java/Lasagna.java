7833c83e414f5756c56c391f8a0c2e1d0b8197a0
Jlee108 <jbacked@yahoo.com>
2022-02-14 16:55:20 +0000

    Steps one and two

---
+++ src/main/java/Lasagna.java
@@ -0,0 +1,13 @@
+public class Lasagna {
+    public static int expectedMinutesInOven() {
+        return 40;
+    }
+
+    public static int remainingMinutesInOven() {
+        return Lasagna.expectedMinutesInOven();
+    }
+
+    // TODO: define the 'preparationTimeInMinutes()' method
+
+    // TODO: define the 'totalTimeInMinutes()' method
+}
